d4651e340b7b3b56026dd8d6f29086ecdd89e6cd
Irene Seedorff <ireneseedorff@Brugers-MacBook-Pro-3.local>
2018-10-31 14:05:50 +0100

    Settingknap vises

--- script.js
+++ script.js
@@ -8,16 +8,24 @@ function sidenVises() {
     showStart();
 }
 
+// START SPIL
+
 function showStart() {
     console.log("showStart");
     //Vis startskærm
+    document.querySelector("#start").classList.remove("hide");
 
     document.querySelector("#start").classList.add("show");
     // Start animation på startskærm
     document.querySelector("#play").classList.add("pulse");
 
-    //click
+    //click to play
     document.querySelector("#play").addEventListener("click", hideStart);
+
+    //click to settings
+    document.querySelector("#settings_button").addEventListener("click", showSettings);
+
+
 }
 
 function hideStart() {
@@ -32,12 +40,29 @@ function hideStart() {
     document.querySelector("#start").addEventListener("animationend", startGame);
 }
 
+
 function startGame() {
     console.log("startGame");
+    document.querySelector("#start").removeEventListener("animationend", startGame);
     // Skjul startskærm
     document.querySelector("#start").classList.add("hide");
     // Vis spilskærm
     document.querySelector("#game").classList.add("show");
+    document.querySelector("#game_ui").classList.remove("hide");
+}
 
 
+// SETTING KNAP
+
+function showSettings() {
+    console.log("showSettings");
+    document.querySelector("#settings_button").removeEventListener("click", showSettings);
+    document.querySelector("#settings").classList.remove("hide");
+
+    // Stop animation på startskærm
+    document.querySelector("#settings_button").classList.add("pulse");
+    //Fade startskærm ud
+    document.querySelector("#start").classList.add("fade_out");
+    // Vis settings
+    document.querySelector("#settings").classList.add("show");
 }
